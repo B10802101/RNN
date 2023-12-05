5dbb5fa50bbb724e41707e5475383c3fed6aa2a9
B10802101 <143921835+B10802101@users.noreply.github.com>
2023-12-05 09:11:44 +0800

    Update RNN.py
    20231205

--- RNN.py
+++ RNN.py
@@ -8,7 +8,9 @@ import time
 import matplotlib.pyplot as plt
 from torch.optim import lr_scheduler
 from sklearn.model_selection import KFold
-
+print(torch.__version__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(torch.cuda.is_available())
 datapath = './data.npz'
 MFCC_feature = 13
 epochs = 200
@@ -16,22 +18,26 @@ learningrate = 0.0005
 batchsize = 20
 n_fold = 5
 earlystop = 40
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 class RNN_model(nn.Module):
     def __init__(self):
         super().__init__()
-        self.lstm1 = nn.LSTM(input_size=MFCC_feature, hidden_size=64, num_layers=1, batch_first=True)
-        self.linear = nn.Linear(64, 36)
+        self.hidden_state_history = []
+        self.lstm1 = nn.LSTM(input_size=MFCC_feature, hidden_size=128, num_layers=2, batch_first=True)
+        self.dropout = nn.Dropout(p = 0.2)
+        self.linear = nn.Linear(128, 36)
     def forward(self, x):
+        x = x.to(device)
         x, _ = self.lstm1(x)
-        x = x[:, -1, :]                             # shape of x = batch size * 64
+        x = x[:, -1, :]                             # shape of x = batch size * 128
+        self.hidden_state_history = x
         x = nn.functional.leaky_relu(x)
+        x = self.dropout(x)
         x = self.linear(x)
         return x
 
 def load_dataset(filepath):
-    data = np.load(filepath)
+    data = np.load(filepath, allow_pickle=True)
     X = data['MFCCs'].astype('float')
     Y = data['labels'].astype('float')
     print('Data sets loaded!')
@@ -50,33 +56,42 @@ def batch_the_data(batchsize, X_train, X_test, Y_train, Y_test):
     return X_train_batch, X_test_batch, Y_train_batch, Y_test_batch
 
 def main():
-
+    with open('data.json', 'r') as f:
+      data = json.load(f)
+    numpy_array1 = np.array(data['labels'])
+    numpy_array2 = np.array(data['MFCCs'])
+    numpy_array3 = np.array(data['files'])
+    np.savez('data.npz', labels = numpy_array1, MFCCs = numpy_array2, files = numpy_array3)
+    
     # Split the data
     X_train, X_test, Y_train, Y_test = get_data_splits(datapath, 0.1)
     X_train_batch, X_test_batch, Y_train_batch, Y_test_batch = batch_the_data(batchsize, X_train, X_test, Y_train, Y_test)
 
 
     # To tensor & GPU
-    X_train_batch = torch.Tensor(X_train_batch)
-    X_test_batch = torch.Tensor(X_test_batch[:len(X_test_batch)-1])
-    Y_train_batch = torch.Tensor(Y_train_batch)
-    Y_test_batch = torch.Tensor(Y_test_batch[:len(Y_test_batch)-1])
+    X_train_batch = torch.Tensor(X_train_batch).to(device)
+    X_test_batch = torch.Tensor(X_test_batch[:len(X_test_batch)-1]).to(device)
+    Y_train_batch = torch.Tensor(Y_train_batch).to(device)
+    Y_test_batch = torch.Tensor(Y_test_batch[:len(Y_test_batch)-1]).to(device)
 
     # Set the model
     model = RNN_model()
+    model.to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr = learningrate)
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.3)
+    # scheduler = lr_scheduler.StepLR(optimizer, step_size=30, gamma=0.3)
     kf = KFold(n_splits=n_fold, shuffle=True, random_state=4)
 
     # Start training
-    training_loss, validation_loss, training_accuracys, validation_accuracys = [], [], [], []
+    # model.load_state_dict(torch.load('best_model.pth'))
+    training_loss, validation_loss, training_accuracys, validation_accuracys, lr_history = [], [], [], [], []
     best_accuracy = 0
     for fold, (train_index, val_index)in enumerate(kf.split(X_train_batch)):
-        X_train_batch_fold = X_train_batch[train_index].to(device)
-        Y_train_batch_fold = Y_train_batch[train_index].to(device)
-        X_validation_batch_fold = X_train_batch[val_index].to(device)
-        Y_validation_batch_fold = Y_train_batch[val_index].to(device)
+        X_train_batch_fold = X_train_batch[train_index]
+        Y_train_batch_fold = Y_train_batch[train_index]
+        X_validation_batch_fold = X_train_batch[val_index]
+        Y_validation_batch_fold = Y_train_batch[val_index]
+        earlystop_patience = 0
         for epoch in range(epochs):
 
             # Training
@@ -86,7 +101,7 @@ def main():
             total_train = 0
             correct_recognized_val = 0
             total_validation =0
-            earlystop_patience = 0
+            model.train()
             for X_train_subbatch, Y_train_subbatch in zip(X_train_batch_fold, Y_train_batch_fold):
                 X_train_subbatch.to(device)
                 Y_train_subbatch.to(device)
@@ -106,6 +121,10 @@ def main():
                 iter +=1
                 if (iter + 1) % (1000) == 0:
                     print(f'            {fold + 1} fold    {epoch + 1}/{epochs} epoch    {iter + 1}/{len(Y_train_batch_fold)}   loss: {loss.item():.4f}')
+                if iter == 3000:
+                    with open('hidden_state_history.txt', 'w') as f:
+                        for element in model.hidden_state_history:
+                            f.write(str(element) + ' ')
             training_loss.append(loss.item())
             training_accuracys.append(training_accuracy)
 
@@ -127,6 +146,8 @@ def main():
                     best_accuracy = correct_recognized_val / total_validation
                     print('Saving better model parameters...')
                     torch.save(model.state_dict(), 'best_model.pth')
+                    if fold == 0:
+                        torch.save(model.state_dict(), 'best_model_fold0.pth')
                 else:
                     earlystop_patience += 1
                     if earlystop_patience == earlystop:
@@ -138,24 +159,49 @@ def main():
             print(f'            Training accuracy {(training_accuracy) * 100}%    Validation accuracy: {(validation_accuracy) * 100}%')
             print(f'            Training time of {epoch + 1} epoch is {time.time() - start_time}')
             print('//////////////////////////////////////////////////////////')
+            
             #Update learning rate
-            scheduler.step()
+            
+            # lr_history.append(learningrate)
+            # scheduler.step()
     print('====================Finish training=========================')
+    
+    # Testing
+    model.eval()
+    correct_recognized_test = 0
+    total_test = 0
+    with torch.no_grad():
+        for X_test_subbatch, Y_test_subbatch in zip(X_test_batch, Y_test_batch):
+            output = model(X_test_subbatch)
+            _, test_predict = torch.max(output, 1)
+            for i in range(0, batchsize):
+                if test_predict[i] == Y_test_subbatch[i]:
+                    correct_recognized_test += 1
+            total_test += Y_test_subbatch.size(0)
+        test_accuracy = (correct_recognized_test / total_test) * 100
+    print(f'Test accuracy = {test_accuracy}')
 
     #Show the Training history
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize = (10, 8))
-    ax1.plot(range(len(training_loss)), training_loss, 'b', label = 'training loss')
-    ax1.plot(range(len(validation_loss)), validation_loss, 'g', label = 'validation loss')
-    ax1.set_xlabel('Number of steps')
-    ax1.set_ylabel('Loss')
-    ax1.legend()
+    plt.subplot(2, 2, 1)
+    plt.plot(range(len(training_loss)), training_loss, 'b', label = 'training loss')
+    plt.plot(range(len(validation_loss)), validation_loss, 'g', label = 'validation loss')
+    plt.xlabel('Number of steps')
+    plt.ylabel('Loss')
+    plt.legend()
 
-    ax2.plot(range(len(validation_accuracys)), validation_accuracys, 'g', label = 'validation accuracy')
-    ax2.plot(range(len(training_accuracys)), training_accuracys, 'b', label = 'training accuracy')
-    ax2.set_xlabel('Number of steps')
-    ax2.set_ylabel('Accuracy')
-    ax2.legend()
+    plt.subplot(2, 2, 2)
+    plt.plot(range(len(validation_accuracys)), validation_accuracys, 'g', label = 'validation accuracy')
+    plt.plot(range(len(training_accuracys)), training_accuracys, 'b', label = 'training accuracy')
+    plt.xlabel('Number of steps')
+    plt.ylabel('Accuracy')
+    plt.legend()
     plt.show()
+
+    # plt.subplot(2, 2, 3)
+    # plt.plot(range(lr_history), lr_history)
+    # plt.xlabel('Number of steps')
+    # plt.title('Learningrate History')
+    # plt.show()
 
 if __name__ == '__main__':
     main()
